dc0494b79788da428a8bb1986815b7ef125ae8f9
wheesunglee <wlee9486@naver.com>
2024-03-07 13:58:58 +0900

    [UA-42] feat:  add exception filter\n

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './interceptors/transform.interceptor';
+import { HttpExceptionFilter } from './filters/httpException.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 5051;
 
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalInterceptors(new TransformInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(port, () => {
     console.log(`[CultureLand_BE] Server is running at port : ${port}`);
